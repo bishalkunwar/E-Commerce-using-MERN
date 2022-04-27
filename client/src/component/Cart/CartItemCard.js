f7bc3c9279266fa717853d65007ef016311d2338
bishalkunwar <bishal.kunwar487@gmail.com>
2022-04-27 09:11:05 +0545

    cart component designed and developed

--- client/src/component/Cart/CartItemCard.js
+++ client/src/component/Cart/CartItemCard.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import "./CartItemCard.css";
+import {Link} from "react-router-dom";
 
-const CartItemCard = () => {
+const CartItemCard = ({item, deleteCartItems}) => {
   return (
-    <div>CartItemCard</div>
-  )
-}
+    <div className="CartItemCard">
+      <img src={item.image} alt="ssa" />
+      <div>
+        <Link to={`/product/$item.product`}>{item.name}</Link>
+        <span>
+        {`Price:NRS.${item.price}`}
+        </span>
+        <p onClick={()=>deleteCartItems(item.product)}>Remove</p>
+      </div>
+    </div>
+  );
+};
 
 export default CartItemCard;
