85223ba155d6d77d378408e0328160c5058ef794
bishalkunwar <bishal.kunwar487@gmail.com>
2022-05-04 20:14:18 +0545

    MyOrders component going on

---
+++ client/src/component/Order/MyOrders.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import "./MyOrders.css";
+
+const MyOrders = () => {
+  return (
+    <Fragment>
+        <div className="myOrdersContainer">
+            My Orders.
+        </div>
+    </Fragment>
+  );
+};
+
+export default MyOrders;
